9134d04e6017ed62ca2a788a15f7336ba4a9566e
Taeyoung Won <hexter1994@gmail.com>
2024-02-27 16:29:43 +0900

    [Build] 플랫폼 버전, 패키지 의존성 설정

--- Package.swift
+++ Package.swift
@@ -4,20 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "KazAlamofire",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "KazAlamofire",
-            targets: ["KazAlamofire"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "KazAlamofire"),
-        .testTarget(
-            name: "KazAlamofireTests",
-            dependencies: ["KazAlamofire"]),
-    ]
+  name: "KazAlamofire",
+  platforms: [
+    .iOS(.v13)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "KazAlamofire",
+      targets: ["KazAlamofire"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Alamofire/Alamofire", from: Version(5, 8, 1))
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "KazAlamofire",
+      dependencies: [
+        .product(name: "Alamofire", package: "Alamofire")
+      ]),
+    .testTarget(
+      name: "KazAlamofireTests",
+      dependencies: ["KazAlamofire"]),
+  ]
 )
