9ce61c172050980002210d59e463848e93d65853
Taeyoung Won <hexter1994@gmail.com>
2024-03-02 01:11:24 +0900

    [Feat] 사용자 정의 HTTP 상태코드 핸들링 함수 추가

--- Sources/KazAlamofire/AFManager.swift
+++ Sources/KazAlamofire/AFManager.swift
@@ -2,6 +2,8 @@ import Alamofire
 
 public final class AFManager {
   
+  public typealias HTTPStatusCode = Int
+  
   // MARK: - Singleton
   public static let shared = AFManager()
   private init() { }
@@ -53,10 +55,36 @@ public final class AFManager {
     responseType: D.Type,
     router: AFRouter
   ) async throws -> D {
-    
     return try await AF.request(router)
       .validate()
       .serializingDecodable(responseType.self)
       .value
+  }
+  
+  public func callRequest<D: Decodable>(
+    responseType: D.Type,
+    router: AFRouter,
+    additionalError: [HTTPStatusCode: Error] = [:]
+  ) async throws -> D {
+    let response = await AF.request(router)
+      .validate()
+      .serializingDecodable(responseType.self)
+      .response
+    
+    guard let httpResponse = response.response else {
+      throw AFError.responseValidationFailed(reason: .customValidationFailed(error: AFManagerError.invalidResponse))
+    }
+    
+    for (statusCode, error) in additionalError {
+      guard httpResponse.statusCode != statusCode else {
+        throw error
+      }
+    }
+    
+    guard let result = response.value else {
+      throw AFError.responseValidationFailed(reason: .customValidationFailed(error: AFManagerError.invalidData))
+    }
+    
+    return result
   }
 }
